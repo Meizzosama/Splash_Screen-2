752e15f0e65cd3a7903987efd3c59e598d93ade3
Muhammad Osama Noor <100303780+Meizzosama@users.noreply.github.com>
2022-12-04 20:43:19 +0500

    Update splash_screen.dart

--- splash_screen.dart
+++ splash_screen.dart
@@ -1,29 +1,44 @@
 import 'package:moxi_mart/consts/consts.dart';
 import '../../widgets_common/applogo_widgets.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.greenAccent,
       body: Center(
-child: Column(
-  children: [
-    Align(
-      alignment: Alignment.topLeft,
-        child: Image.asset(icSplashBg,width: 300,)),
-    50.heightBox,
-    applogoWidget(),
-    10.heightBox,
-    appname.text.fontFamily(bold).size(20).make(),
-    5.heightBox,
-    tagline.text.fontFamily(semibold).size(15).make(),
-    const Spacer(),
-    credits.text.fontFamily(bold).size(20).make(),
-    30.heightBox,
-  ],),),);}}
+        child: Column(
+          children: [
+            Align(
+                heightFactor: 1.5,
+                alignment: Alignment.center,
+                child: Image.asset(
+                  icSplashBg,
+                  width: 300,
+                )),
+            30.heightBox,
+            applogoWidget(),
+            1.heightBox,
+            appname.text
+                .fontFamily(bold)
+                .size(23)
+                .color(Colors.deepPurpleAccent)
+                .make(),
+            7.heightBox,
+            tagline.text.fontFamily(bold).color(Colors.pink).size(18).make(),
+            const Spacer(),
+            credits.text.fontFamily(bold).color(Colors.indigo).size(20).make(),
+            30.heightBox,
+          ],
+        ),
+      ),
+    );
+  }
+}
 //Splash Screen is Completed!!
